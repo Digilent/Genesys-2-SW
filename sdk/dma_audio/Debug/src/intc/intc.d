af4e078c16b9bbfeb45e1aead63fa46b813c9681
KissBorbala <kissborbala01@gmail.com>
2022-09-06 16:14:00 +0300

    Import commit

---
+++ sdk/dma_audio/Debug/src/intc/intc.d
@@ -0,0 +1,42 @@
+src/intc/intc.o src/intc/intc.o: ../src/intc/intc.c ../src/intc/intc.h \
+ ../../dma_audio_bsp/microblaze_0/include/xstatus.h \
+ ../../dma_audio_bsp/microblaze_0/include/xil_types.h \
+ ../../dma_audio_bsp/microblaze_0/include/xil_assert.h \
+ ../../dma_audio_bsp/microblaze_0/include/xintc.h \
+ ../../dma_audio_bsp/microblaze_0/include/xparameters.h \
+ ../../dma_audio_bsp/microblaze_0/include/xstatus.h \
+ ../../dma_audio_bsp/microblaze_0/include/xintc_l.h \
+ ../../dma_audio_bsp/microblaze_0/include/xil_io.h \
+ ../../dma_audio_bsp/microblaze_0/include/xil_printf.h \
+ ../../dma_audio_bsp/microblaze_0/include/bspconfig.h \
+ ../../dma_audio_bsp/microblaze_0/include/mb_interface.h \
+ ../../dma_audio_bsp/microblaze_0/include/xil_exception.h \
+ ../../dma_audio_bsp/microblaze_0/include/xparameters.h
+
+../src/intc/intc.h:
+
+../../dma_audio_bsp/microblaze_0/include/xstatus.h:
+
+../../dma_audio_bsp/microblaze_0/include/xil_types.h:
+
+../../dma_audio_bsp/microblaze_0/include/xil_assert.h:
+
+../../dma_audio_bsp/microblaze_0/include/xintc.h:
+
+../../dma_audio_bsp/microblaze_0/include/xparameters.h:
+
+../../dma_audio_bsp/microblaze_0/include/xstatus.h:
+
+../../dma_audio_bsp/microblaze_0/include/xintc_l.h:
+
+../../dma_audio_bsp/microblaze_0/include/xil_io.h:
+
+../../dma_audio_bsp/microblaze_0/include/xil_printf.h:
+
+../../dma_audio_bsp/microblaze_0/include/bspconfig.h:
+
+../../dma_audio_bsp/microblaze_0/include/mb_interface.h:
+
+../../dma_audio_bsp/microblaze_0/include/xil_exception.h:
+
+../../dma_audio_bsp/microblaze_0/include/xparameters.h:
